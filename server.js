a2145380732da7bcc3d0694e78c4059bdd0bf01e
rfa435 <abhifabhian@outlook.com>
2024-12-27 10:48:54 +0800

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 80;
 
 const db = new sqlite3.Database('./itineraries.db', (err) => {
   if (err) {
